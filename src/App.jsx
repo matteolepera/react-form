c9294535c32096651331b1c63fe21730360d1feb
matteolpera <matteo.lepera02@gmail.com>
2025-12-01 17:04:21 +0100

    feat: milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,20 @@ function App() {
     "AI nel marketing: come far lavorare le macchine mentre tu prendi un caffè",
     "Le skill più richieste nel 2025: ovvero come non farsi superare dai robot"
   ]);
+  const [newArticles, setNewArticles] = useState('');
 
+  function addArticle(event) {
+    event.preventDefault();
+    setArticles([...articles, newArticles]);
+    setNewArticles('');
+  }
 
   return (
     <>
       <header>
         <nav className="navbar bg-primary" data-bs-theme="dark">
           <div className="container-fluid">
-            <a className="navbar-brand" href="#">Blog Articoli Esotici</a>
+            <a className="navbar-brand" href="#">Articoli Esotici</a>
           </div>
         </nav>
       </header>
@@ -27,14 +33,30 @@ function App() {
       <main>
         <div className="container py-3">
           {/* <p>{JSON.stringify(articles)}</p> */}
-          <ul className='list-group list-group-flush'>
-            {articles.map((article, index) => {
-              return (
-                <li key={index} className='list-group-item'><h4>{article}</h4></li>
-              )
-            })
-            }
-          </ul>
+          <section>
+            <ul className='list-group list-group-flush'>
+              {articles.map((article, index) => {
+                return (
+                  <li key={index} className='list-group-item'><h4>{article}</h4></li>
+                )
+              })
+              }
+            </ul>
+          </section>
+
+          <section className='mt-4'>
+
+            <form className='row' onSubmit={addArticle}>
+              <div className="col">
+                <label for="article" className="form-label"><h5>Inserisci Articolo:</h5></label>
+                <input type="text" id="article" className="form-control" value={newArticles}
+                  onChange={(event) => { setNewArticles(event.target.value) }} />
+                <button type="submit" className='btn btn-primary mt-3'>Aggiungi</button>
+              </div>
+            </form>
+
+          </section>
+
         </div>
       </main>
 
