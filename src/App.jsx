08176cf8c9a8b32ee3e01e6afa8fe7b8221f8b72
matteolpera <matteo.lepera02@gmail.com>
2025-12-01 14:58:09 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+function App() {
+
+
+  return (
+    <div className="containter">
+      <button className='btn btn-primary'>Ok</button>
+    </div>
+  )
+}
+
+export default App
